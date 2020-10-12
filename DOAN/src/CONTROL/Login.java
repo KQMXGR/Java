d73e57367d018196afeab30b9bb658d0b0ddd8ab
maixuany <maixuanytp3@gmail.com>
2020-10-12 13:01:20 +0700

    UPDATE

--- DOAN/src/CONTROL/Login.java
+++ DOAN/src/CONTROL/Login.java
@@ -1,4 +1,24 @@
 package CONTROL;
 
-public class Login {
+import javax.swing.*;
+
+public class Login extends JFrame {
+    private JPanel panelLogin;
+    private JButton buttonLogin;
+    private JTextField textFieldUsername;
+    private JPasswordField passwordFieldPassword;
+
+    public Login(){
+        super("Đăng Nhập");
+        this.setContentPane(this.panelLogin);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }
+
 }
