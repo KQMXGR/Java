c53c2acf8eaa65b00209fdc543cea3dd6fa825f5
maixuany <maixuanytp3@gmail.com>
2020-10-13 22:17:46 +0700

    update 10:17 13/10/2020

---
+++ DOAN/src/CONTROL/EditAdmin.java
@@ -0,0 +1,13 @@
+package CONTROL;
+
+import javax.swing.*;
+
+public class EditAdmin {
+    private JPanel panelEditAdmin;
+    private JTextField textFieldUsername;
+    private JPasswordField passwordFieldOldPassword;
+    private JPasswordField passwordFieldReNewPassword;
+    private JPasswordField passwordFieldNewPassword;
+    private JButton buttonAccept;
+    private JButton buttonCancel;
+}
